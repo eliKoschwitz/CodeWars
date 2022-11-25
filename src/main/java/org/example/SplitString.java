2ecd9b80da00a13e1e9a3736b62acbb25ba2f159
elikoschwitz <e.koschwitz@googmail.com>
2022-11-25 15:27:29 +0100

    Remove Main

---
+++ src/main/java/org/example/SplitString.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class SplitString {
+
+    public static String[] solution(String s) {
+
+        //Write your code here
+        char s = s.toCharArray();
+        String[] arr = new String[4];
+
+
+        int var = arr.length();
+    return arr;
+    }
+}
